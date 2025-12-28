7e6b46b8520190c1dee156e9612fa566c21d23db
Mahanama899 <23sea027@stu.cmb.ac.lk>
2025-12-28 20:42:10 +0530

    Update script.js

--- asset/scripts/script.js
+++ asset/scripts/script.js
@@ -10,6 +10,9 @@ const deleteProducts = async() => {
     for (let product of products) {
         const response = await axios.delete(`https://lionfish-app-oy7gr.ondigitalocean.app/product`)
     }
+    setTimeout(() => {
+    window.location.href = window.location.href + "?t=" + Date.now();
+    }, 300);
     location.reload();
     window.scroll({
         top: 0,
